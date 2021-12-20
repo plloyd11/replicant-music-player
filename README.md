b5a5a9e0f243762bf1060c21f1598b69b6902d02
Pete Lloyd <plloyd11@gmail.com>
2021-12-20 12:29:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Replicant Music Player
+
+Music player built using Vue, Howler.js, TailwindCSS, and Vite
+
+- [Howler.js](https://github.com/goldfire/howler.js)
+- [Vue 3](https://v3.vuejs.org/guide/introduction.html)
+- [Tailwind CSS v3](https://tailwindcss.com/docs/configuration)
+- [Vite](https://vitejs.dev/guide/)
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Fira Code"', ...defaultTheme.fontFamily.sans]
+            }
+        }
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio')
+    ]
+};
